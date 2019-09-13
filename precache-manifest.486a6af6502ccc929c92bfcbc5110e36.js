e4140e5581a44f51b36542b4f14c585ebbc94da8
aamjadiqbal <aamjad.iqbal@yahoo.com>
2019-09-13 16:11:46 +0500

    Updates

---
+++ precache-manifest.486a6af6502ccc929c92bfcbc5110e36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f49e2ffe31a3ed9c5dedb492a81e02f3",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "dba083ccf7204f632fb6",
+    "url": "/contactmanager/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "891900012e9943ad1712",
+    "url": "/contactmanager/static/css/main.4f170de8.chunk.css"
+  },
+  {
+    "revision": "dba083ccf7204f632fb6",
+    "url": "/contactmanager/static/js/2.08670c22.chunk.js"
+  },
+  {
+    "revision": "891900012e9943ad1712",
+    "url": "/contactmanager/static/js/main.645cec36.chunk.js"
+  },
+  {
+    "revision": "c37991328db47c672cd9",
+    "url": "/contactmanager/static/js/runtime~main.d8af6e69.js"
+  }
+]);
